bc308a270e663a4f61bd7c93a5443646137127a9
West-Ridge-Brandon <101998379+West-Ridge-Brandon@users.noreply.github.com>
2026-06-10 13:55:55 -0500

    more whoops

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,16 +2,16 @@
 # Outputs
 ############################################
 
-output "VPC Name"              { value = google_compute_network.vpc.name }
+output "VPC-Name"              { value = google_compute_network.vpc.name }
 
-output "Subnet Name"           { value = google_compute_subnetwork.subnet.name }
+output "Subnet-Name"           { value = google_compute_subnetwork.subnet.name }
 
-output "Firewall Rule Name"    { value = google_compute_firewall.firewall.name }
+output "Firewall-Rule-Name"    { value = google_compute_firewall.firewall.name }
 
-output "Instance Name"         { value = google_compute_instance.vm.name }
+output "Instance-Name"         { value = google_compute_instance.vm.name }
 
-output "Instance External IP"  { value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip }
+output "Instance-External-IP"  { value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip }
 
-output "Bucket Name"           { value = google_storage_bucket.bucket.name }
+output "Bucket-Name"           { value = google_storage_bucket.bucket.name }
 
-output "Service Account Email" { value = google_service_account.vm_sa.account_id }
+output "Service-Account-Email" { value = google_service_account.vm_sa.account_id }
